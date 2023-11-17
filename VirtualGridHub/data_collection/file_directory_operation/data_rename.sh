324b014f68bb7fbbc15369667d3a609cc4a78104
wdtkm0823 <takumi.10930823@gmail.com>
2023-11-17 18:23:40 +0900

    [update] 名前変更するためにパスの変更

--- VirtualGridHub/data_collection/file_directory_operation/data_rename.sh
+++ VirtualGridHub/data_collection/file_directory_operation/data_rename.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # 対象ディレクトリのパスを指定します
-directory="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_pdnego_20231024/train/cheeropowerplus5#1/"
+directory="/home/pi/Desktop/doyer/research/VirtualGridHub/main/sampling_distribution/python/output/"
 
 # ディレクトリ内のファイルを順に処理します
-for file in "$directory"*xperiaxz2compact#1*; do
-    # ファイル名から"xperiaxz2compact#1"を"cheeropowerplus5#1"に置換して変更します
-    new_filename=$(echo "$file" | sed 's/xperiaxz2compact#1/cheeropowerplus5#1/')
+for file in "$directory"*validation*; do
+    new_filename=$(echo "$file" | sed 's/validation/test/')
     mv "$file" "$new_filename"
     echo "ファイル名を変更しました: $file を $new_filename に変更しました。"
 done
