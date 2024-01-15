5d58cfca2d7ad74a45c6b10d45d1b4bc575cb73a
takumi <twada698@gmail.com>
2024-01-15 16:36:40 +0900

    clean add comments

--- VirtualGridHub/data_collection/file_directory_operation/data_rename.sh
+++ VirtualGridHub/data_collection/file_directory_operation/data_rename.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# 対象ディレクトリのパスを指定します
+# Specify the path of the target directory
 directory="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_pdnego_20231024/cwt/"
 
-# ディレクトリ内のファイルを順に処理します
+# Processes files in directories in sequence
 for file in "$directory"*test*; do
     new_filename=$(echo "$file" | sed 's/test/validation/')
     mv "$file" "$new_filename"
-    echo "ファイル名を変更しました: $file を $new_filename に変更しました。"
+    echo "File name has been changed.: Changed $file to $new_filename."
 done
