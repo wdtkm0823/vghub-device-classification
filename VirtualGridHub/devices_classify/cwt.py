5b2f14e1d24762a71989c5fad961f05410862b11
takumi <twada698@gmail.com>
2024-01-19 09:42:38 +0900

    Clean refactoring

--- VirtualGridHub/devices_classify/cwt.py
+++ VirtualGridHub/devices_classify/cwt.py
@@ -9,18 +9,18 @@ from pandas.plotting import register_matplotlib_converters
 import glob
 from scipy import signal
 
-print("15s or pdnego : ", end="")
-file = input()
+print("which method, 15s or pdnego? : ", end="")
+method = input()
 
-print("test or validation or train : ", end="")
-data = input()
+print("Which data set is it, test or validation or train? : ", end="")
+dataset = input()
 
-print("cheeropowerplus5 or cheeropowermountain or googlepixel3a or ipadair4th or xperiaxz2compact : ", end="")
+print("Which device is it, cheeropowerplus5 or cheeropowermountain or googlepixel3a or ipadair4th or xperiaxz2compact? : ", end="")
 device = input()
 
-ROOT_DIR = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_{file}_20231024/cwt'
+ROOT_DIR = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_{method}_20231024/cwt'
 TARGET_PATTERN = "**.csv"
-SAVEPATH = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_cwt/dataset_{file}_20231024/{data}/{device}#1'
+SAVEPATH = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_cwt/dataset_{method}_20231024/{dataset}/{device}#1'
 
 register_matplotlib_converters()
 sns.set(style='whitegrid', palette='muted', font_scale=1.5)
@@ -38,7 +38,7 @@ def make_cwt_dataset(data_path, save_a_path):
         plot_cwt_save(cwt_arr,figure_size,save_a_path,save_filename)
     print('CWT DATASET DONE...')
 
-# 一つのcsvファイルから VBUS V I W  を返す
+# 一つのcsvファイルからVBUS V I Wを返す
 def ImportCSVandConvertDF(FILE):
     data=pd.read_csv(FILE)
     data_V=data.iloc[:,1]
@@ -52,7 +52,7 @@ def ImportCSVandConvertDF(FILE):
     npdata_W
     return npdata_W
 
-# ricker ウェーブレット
+# rickerウェーブレット
 def calcuate_cwt_ricker(sig):
     widths = np.arange(1, 31)
     cwtmatr = signal.cwt(sig, signal.ricker, widths)
@@ -71,7 +71,7 @@ def cwt():
     root_a_path=[]
     root_a_path.extend(glob.glob(os.path.join(ROOT_DIR, TARGET_PATTERN)))
     root_a_test_path = root_a_path
-    #csv から　cwt の画像を生成し，データセットに保存
+    #csvからcwtの画像を生成し、データセットに保存
     make_cwt_dataset(root_a_test_path, SAVEPATH)
     filename = glob.glob(os.path.join(SAVEPATH, '*.png'))
 cwt()
