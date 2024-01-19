0ae1c00bfe65e12d733f2341f4014faea60a11bc
takumi <twada698@gmail.com>
2024-01-19 10:28:52 +0900

    Clean refactoring

--- VirtualGridHub/data_collection/file_directory_operation/1_data_mv_output_to_cwt.sh
+++ VirtualGridHub/data_collection/file_directory_operation/1_data_mv_output_to_cwt.sh
@@ -3,10 +3,10 @@
 # The folder from which to move the file.
 source_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/main/sampling_distribution/python/output"
 
-read -p "15s or pdnego : " file
+read -p "which method it it, 15s or pdnego? : " method
 
 # The folder to which to move the file.
-destination_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_${file}_20231024/cwt"
+destination_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_${method}_20231024/cwt"
 
 # Move files matching the pattern to the destination folder.
-mv "$source_folder"/output_vghub_"$file"_data_3*.csv "$destination_folder"
+mv "$source_folder"/output_vghub_"$method"_data_3*.csv "$destination_folder"
--- VirtualGridHub/data_collection/file_directory_operation/2_data_mv_cwt_to_eval_csv.sh
+++ VirtualGridHub/data_collection/file_directory_operation/2_data_mv_cwt_to_eval_csv.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-read -p "15s or pdnego : " file
+read -p "which method it it, 15s or pdnego? : " method
 
 # The folder from which to move the file.
-source_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_${file}_20231024/cwt"
+source_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_${method}_20231024/cwt"
 
-read -p "test or validation or train : " data
+read -p "Which data set is it, test or validation or train? : " dataset
 
-read -p "cheeropowerplus5 or or cheeropowermountain or googlepixel3a or ipadair4th or xperiaxz2compact : " device
+read -p "Which device is it, cheeropowerplus5 or cheeropowermountain or googlepixel3a or ipadair4th or xperiaxz2compact? : " device
 
 # The folder to which to move the file.
-destination_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_${file}_20231024/${data}/${device}#1"
+destination_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_${method}_20231024/${dataset}/${device}#1"
 
 # Move files matching the pattern to the destination folder.
-mv "$source_folder"/output_vghub_"$file"_data_3*.csv "$destination_folder"
+mv "$source_folder"/output_vghub_"$method"_data_3*.csv "$destination_folder"
