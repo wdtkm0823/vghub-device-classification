dfdebf7d58c3a448d51011d0581b29e5ec634a0b
takumi <twada698@gmail.com>
2024-01-19 09:28:01 +0900

    Clean refactoring

--- VirtualGridHub/devices_classify/cwt.py
+++ VirtualGridHub/devices_classify/cwt.py
@@ -10,17 +10,17 @@ import glob
 from scipy import signal
 
 print("15s or pdnego : ", end="")
-method = input()
+file = input()
 
 print("test or validation or train : ", end="")
-data_set = input()
+data = input()
 
 print("cheeropowerplus5 or cheeropowermountain or googlepixel3a or ipadair4th or xperiaxz2compact : ", end="")
 device = input()
 
-ROOT_DIR = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_{method}_20231024/cwt'
+ROOT_DIR = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_{file}_20231024/cwt'
 TARGET_PATTERN = "**.csv"
-SAVEPATH = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_cwt/dataset_{method}_20231024/{data_set}/{device}#1'
+SAVEPATH = f'/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_cwt/dataset_{file}_20231024/{data}/{device}#1'
 
 register_matplotlib_converters()
 sns.set(style='whitegrid', palette='muted', font_scale=1.5)
@@ -30,15 +30,15 @@ rcParams['figure.figsize'] = 22, 10
 #データセット分の画像を生成
 def make_cwt_dataset(data_path, save_a_path):
     figure_size=(50,50)
-    for d in enumerate(data_path):
+    for j, d in enumerate(data_path):
         w = ImportCSVandConvertDF(d)
         cwt_arr=calcuate_cwt_ricker(w)
         length_path = len(str(d).split("/"))
         save_filename = str(d).split("/")[length_path-1].split(".csv")[0]+"_ricker"
         plot_cwt_save(cwt_arr,figure_size,save_a_path,save_filename)
-    print('CWT DATASET DONE')
+    print('CWT DATASET DONE...')
 
-# １つのcsvファイルからVBUS V I W を返す
+# 一つのcsvファイルから VBUS V I W  を返す
 def ImportCSVandConvertDF(FILE):
     data=pd.read_csv(FILE)
     data_V=data.iloc[:,1]
@@ -52,14 +52,14 @@ def ImportCSVandConvertDF(FILE):
     npdata_W
     return npdata_W
 
-# rickerウェーブレット
+# ricker ウェーブレット
 def calcuate_cwt_ricker(sig):
     widths = np.arange(1, 31)
     cwtmatr = signal.cwt(sig, signal.ricker, widths)
     return cwtmatr
 
 #CWTを画像で保存
-def plot_cwt_save(cwtmatr_,SAVEPATH,FILENAME):
+def plot_cwt_save(cwtmatr_,figure_size,SAVEPATH,FILENAME):
     plt.imshow(cwtmatr_, extent=[-1, 1, 1, 31], cmap='gray', aspect='auto')
     plt.xlabel("Time[s]")
     plt.ylabel("Frequency[Hz]")
@@ -71,7 +71,7 @@ def cwt():
     root_a_path=[]
     root_a_path.extend(glob.glob(os.path.join(ROOT_DIR, TARGET_PATTERN)))
     root_a_test_path = root_a_path
-    #csvからcwtの画像を生成し、データセットに保存
+    #csv から　cwt の画像を生成し，データセットに保存
     make_cwt_dataset(root_a_test_path, SAVEPATH)
     filename = glob.glob(os.path.join(SAVEPATH, '*.png'))
 cwt()
