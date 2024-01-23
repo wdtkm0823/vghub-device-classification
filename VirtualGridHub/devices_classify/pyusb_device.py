038d194b82eac8e185f5b6fcc077a448b5564868
takumi <twada698@gmail.com>
2024-01-23 17:06:12 +0900

    Refactor USB device handling and add device info retrieval

--- VirtualGridHub/devices_classify/pyusb_device.py
+++ VirtualGridHub/devices_classify/pyusb_device.py
@@ -2,40 +2,26 @@
 
 import usb.core
 
-class UsbDevice:
-    def __init__(self, vendor_id=None, product_id=None):
-        self.device = self.find_device(vendor_id, product_id)
-        self.configuration = self.device.configurations[0]
-        self.interface = self.configuration.interfaces[0][0]
-        self.endpoints = [endpoint for endpoint in self.interface.endpoints]
-        self.pipes = [endpoint.address for endpoint in self.endpoints]
+def get_all_usb_devices():
+    devices = []
+    for bus in usb.busses():
+        for device in bus.devices:
+            devices.append(device)
+    return devices
 
-    def find_device(self, vendor_id, product_id):
-        for bus in usb.busses():
-            for device in bus.devices:
-                if (device.idVendor, device.idProduct) == (vendor_id, product_id):
-                    return device
-        raise RuntimeError('Device not found')
-
-    def open(self):
-        if hasattr(self, 'handle'):
-            raise RuntimeError('Device already opened')
-        self.handle = self.device.open()
-        product = self.handle.getString(self.device.iProduct, 20)
-        manufacturer = self.handle.getString(self.device.iManufacturer, 20)
-        print(product, manufacturer)
-
-    def close(self):
-        if hasattr(self, 'handle'):
-            self.handle.releaseInterface()
-            del self.handle
-        else:
-            raise RuntimeError('Device not opened')
+def get_device_info(device):
+    try:
+        handle = device.open()
+        product = handle.getString(device.iProduct, 256)
+        manufacturer = handle.getString(device.iManufacturer, 256)
+        print(f"Product: {product}, Manufacturer: {manufacturer}")
+    finally:
+        handle.release()
 
 def main():
-    dev = UsbDevice(0x04fe, 0x0008)
-    dev.open()
-    dev.close()
+    all_devices = get_all_usb_devices()
+    for device in all_devices:
+        get_device_info(device)
 
 if __name__ == '__main__':
     main()
