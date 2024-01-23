e1d6c58d310a1cca031f3d3dfba35c2d66b664be
takumi <twada698@gmail.com>
2024-01-23 14:18:50 +0900

    Update user prompts in data_mv_cwt_to_eval_csv.sh script

--- VirtualGridHub/data_collection/file_directory_operation/2_data_mv_cwt_to_eval_csv.sh
+++ VirtualGridHub/data_collection/file_directory_operation/2_data_mv_cwt_to_eval_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ask for the method
-echo -e "1: 15s\n2: pdnego"
+echo "1: 15s\n2: pdnego"
 read -p "Enter the option number: " method_number
 
 case $method_number in
@@ -21,7 +21,7 @@ esac
 source_folder="/home/pi/Desktop/doyer/research/VirtualGridHub/devices_classify/eval_csv/dataset_${method}_20231024/cwt"
 
 # Ask for the dataset
-echo -e "1: test\n2: train\n3: validation"
+echo "1: test\n2: train\n3: validation"
 read -p "Enter the option number: " dataset_number
 
 case $dataset_number in
@@ -41,7 +41,7 @@ case $dataset_number in
 esac
 
 # Ask for the device
-echo -e "1: cheeropowermountain\n2: cheeropowerplus5\n3: googlepixel3a\n4: ipadair4th\n5: omnichargeomni20+\n6: xperiaxz2compact"
+echo "1: cheeropowermountain\n2: cheeropowerplus5\n3: googlepixel3a\n4: ipadair4th\n5: omnichargeomni20+\n6: xperiaxz2compact"
 read -p "Enter the option number: " device_number
 
 case $device_number in
