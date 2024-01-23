ee9b62fe8bc5a2bd081d83a1a7776e5ce2e3e59b
takumi <twada698@gmail.com>
2024-01-23 17:15:47 +0900

    Encode product and manufacturer strings if they are not None

--- VirtualGridHub/devices_classify/pyusb_device.py
+++ VirtualGridHub/devices_classify/pyusb_device.py
@@ -14,6 +14,13 @@ def get_device_info(device):
         handle = device.open()
         product = handle.getString(device.iProduct, 256)
         manufacturer = handle.getString(device.iManufacturer, 256)
+
+        # Check if product and manufacturer are not None before encoding
+        if product is not None:
+            product = product.encode('utf-8')
+        if manufacturer is not None:
+            manufacturer = manufacturer.encode('utf-8')
+
         print("Product: {}, Manufacturer: {}".format(product, manufacturer))
     finally:
         handle.releaseInterface()
